f693d87589d67fd24623aee7b39200aa9fdddf80
Sebastian Hernandez <33647914+seabass189@users.noreply.github.com>
2020-09-10 17:09:19 -0700

    Loading items from db and displaying based on category

--- items/models.py
+++ items/models.py
@@ -12,10 +12,10 @@ class Item(models.Model):
     name = models.CharField(max_length=50)
     price = models.DecimalField(decimal_places=2,max_digits=8)
     category_id = models.ForeignKey(Category,on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='images/', default='/static/NoImageFound.jpg')
+    image = models.ImageField(upload_to='images/', default='../images/NoImageFound.jpg')
 
     def __str__(self):
-        return self.name + ", Category: " + str(self.category_id)
+        return self.name + ", Category: " + str(self.category_id.name)
 
     def get_dict_of_model(self):
         return {
--- items/views.py
+++ items/views.py
@@ -3,3 +3,18 @@ from .models import Category, Item
 
 def home(request):
     return render (request, 'items/home.html')
+
+def items_list(request):
+    category_list = Category.objects.all()
+    category_items = []
+    category_names = []
+    for category in category_list:
+        category_names.append(category.name)
+        items = list(Item.objects.filter(category_id=category.id))
+        category_items.append(items)
+    context = {
+    'category_names': category_names,
+    'category_items': category_items,
+    'i_amt': range(len(category_names)),
+    }
+    return render (request, 'items/items_list.html', context=context)
