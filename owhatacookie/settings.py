248bbcf7c87af39bc9a2697e559a813c8d2e9fb7
Sebastian Hernandez <33647914+seabass189@users.noreply.github.com>
2020-09-10 20:09:53 -0700

    help

--- owhatacookie/settings.py
+++ owhatacookie/settings.py
@@ -13,7 +13,9 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -77,7 +79,7 @@ WSGI_APPLICATION = 'owhatacookie.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
